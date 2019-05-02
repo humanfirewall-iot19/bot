20a7c5b208b3d84cbe74ec8b0eb632865d0c57d5
Leonardo Sarra <leonardosarra@outlook.com>
2019-05-02 12:41:17 +0200

    Add helper for telegram bot

---
+++ bot_helper.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+
+
+def build_menu(buttons,
+               n_cols,
+               header_buttons=None,
+               footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons)
+    if footer_buttons:
+        menu.append(footer_buttons)
+    return menu
