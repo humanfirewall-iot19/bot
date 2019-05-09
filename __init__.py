373af239d1cd0f406226efac950b3606fa5694d4
Leonardo Sarra <leonardosarra@outlook.com>
2019-05-10 00:35:01 +0200

    Add support for multiple intercoms for user

--- __init__.py
+++ __init__.py
@@ -1,19 +1,19 @@
 import logging
-import os
 from io import BytesIO
 
 import requests
+import telegram
 from PIL import Image
-
 from pyzbar.pyzbar import decode
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler, MessageHandler, Filters
-import telegram
-from .bot_helper import get_url, build_menu
-from .db_helper import DBHelper
 
-REQUEST_NAME_CONFIGURE = 2
-CONFIGURE_RESPONSE = 1
+from bot_helper import get_url, build_menu
+from db_helper import DBHelper
+
+DEVICE_NAME_GET = 2
+DEVICE_ID_GET = 1
+HANDLER_DELETE = 3
 
 
 class Bot:
@@ -22,13 +22,18 @@ class Bot:
     def __init__(self, token):
         self.updater = Updater(token)
         dp = self.updater.dispatcher
+        dp.add_handler(ConversationHandler(
+            entry_points=[CommandHandler('delete', self.delete)],
+            states={
+                HANDLER_DELETE: [CallbackQueryHandler(self._handle_callback_delete)],
+            }, fallbacks=[CommandHandler('cancel', _cancel)]))
         dp.add_handler(CommandHandler('test', self._test_notification))
         dp.add_handler(CommandHandler('help', help))
         dp.add_handler(ConversationHandler(
             entry_points=[CommandHandler('configure', _configure_start)],
             states={
-                REQUEST_NAME_CONFIGURE: [MessageHandler(Filters.text, request_name, pass_user_data=True)],
-                CONFIGURE_RESPONSE: [
+                DEVICE_NAME_GET: [MessageHandler(Filters.text, request_name, pass_user_data=True)],
+                DEVICE_ID_GET: [
                     MessageHandler((Filters.text | Filters.photo), self.received_deviceid, pass_user_data=True)]
             },
             fallbacks=[CommandHandler('cancel', _cancel)]))
@@ -43,17 +48,18 @@ class Bot:
         chat_ids = db.get_chatID_by_device(str(board_id))
         feedback = db.get_feedback_by_target(target_id)
         for chat_id in chat_ids:
+            device_name = db.get_device_name_by_chatID_and_device(chat_id, board_id)
             if url_photo is None:
                 url_photo = get_url()
             button_list = [
-                InlineKeyboardButton("Lascia un feedback", callback_data="Feedback,{}".format(target_id)),
+                InlineKeyboardButton("Lascia un feedback", callback_data="feedback,{}".format(target_id)),
             ]
             reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=1))
             try:
                 self.updater.bot.send_photo(chat_id=chat_id, photo=url_photo, timeout=120)
             except telegram.error.TimedOut:
                 pass
-            text = "Pensiamo che utente {} abbia suonato alla porta!".format(target_id)
+            text = "[{}] Pensiamo che utente {} abbia suonato alla porta!".format(device_name, target_id)
 
             if feedback is None:
                 text += "\nPersona ignota."
@@ -77,11 +83,14 @@ class Bot:
 
     def _handle_callback_feedback(self, bot, update):
         data = str(update.callback_query.data)
+        print(data)
+        if not data.startswith("feedback") and not data.startswith("result_feedback"):
+            pass
         target = data.split(",")[1]
-        if data.startswith("Feedback"):
+        if data.startswith("feedback"):
             button_list = [
-                InlineKeyboardButton("Scammer", callback_data="Scammer,{}".format(target)),
-                InlineKeyboardButton("Not-scammer", callback_data="Not-Scammer,{}".format(target))
+                InlineKeyboardButton("Scammer", callback_data="result_feedback,Scammer,{}".format(target)),
+                InlineKeyboardButton("Not-scammer", callback_data="result_feedback,Not-Scammer,{}".format(target))
             ]
             reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=1))
             update.callback_query.edit_message_text(
@@ -105,6 +114,10 @@ class Bot:
 
     def received_deviceid(self, bot, update, user_data):
         chat_id = str(update.message.chat_id)
+        device_name = user_data["device_name"]
+        if device_name is None:
+            bot.send_message(chat_id=chat_id, text="An unexpected error happened!\nTry again later")
+            pass
         if update.message.text is None and (update.message.photo is None or len(update.message.photo) == 0):
             bot.send_message(chat_id=chat_id, text="Devi scrivere l'id o mandare una foto con un QRCODE!")
             return
@@ -121,12 +134,38 @@ class Bot:
         bot.send_message(chat_id=chat_id, text="Ho correttamente configura l'id {}".format(intercom_id))
         db = DBHelper()
         db.connect()
-        db.add_user(str(intercom_id), str(chat_id))
+        db.add_user(str(intercom_id), str(device_name), str(chat_id))
         db.close()
         return ConversationHandler.END
 
     def _test_notification(self, bot, update):
         self.send_notification("1", "1", None)
+
+    def delete(self, bot, update):
+        chat_id = str(update.message.chat_id)
+        db = DBHelper()
+        db.connect()
+        device_names = db.get_device_names_by_chatID(chat_id)
+        buttons_list = []
+        for device in device_names:
+            buttons_list.append(InlineKeyboardButton(device, callback_data=device))
+        reply_markup = InlineKeyboardMarkup(build_menu(buttons_list, n_cols=1))
+        if len(buttons_list) == 0:
+            update.message.reply_text('No device added were found!')
+            return ConversationHandler.END
+        else:
+            update.message.reply_text('What intercom do you want to remove?', reply_markup=reply_markup)
+        db.close()
+        return HANDLER_DELETE
+
+    def _handle_callback_delete(self, bot, update):
+        device_name = str(update.callback_query.data)
+        db = DBHelper()
+        db.connect()
+        db.delete_user_by_id_and_device_name(device_name, str(update.callback_query.message.chat.id))
+        update.callback_query.edit_message_text(
+            text="Device eliminato correttamente")
+        return ConversationHandler.END
 
 
 def _help(bot, update):
@@ -141,15 +180,23 @@ def _configure_start(bot, update):
     bot.send_message(chat_id=chat_id,
                      text="Inserisci il nome del citofono per iniziare a ricevere le notifiche di chi ti suona alla "
                           "porta")
-    return REQUEST_NAME_CONFIGURE
+    return DEVICE_NAME_GET
 
 
 def request_name(bot, update, user_data):
     text = update.message.text
-    user_data['choice'] = text
+    user_data['device_name'] = text
+    db = DBHelper()
+    db.connect()
+
+    names = db.get_device_names_by_chatID(str(update.message.chat_id))
+    if text in names:
+        update.message.reply_text(
+            'Device name must be unique!\nTry again'.format(text))
+        return DEVICE_NAME_GET
     update.message.reply_text(
-        'Ok what is the id for {}?'.format(text.lower()))
-    return CONFIGURE_RESPONSE
+        'Ok what is the id for {}?'.format(text))
+    return DEVICE_ID_GET
 
 
 def _cancel(bot, update):
