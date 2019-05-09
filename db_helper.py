b051d4c8438390e40e07f9c5f05cfb42fc0b513b
Leonardo Sarra <leonardosarra@outlook.com>
2019-05-10 00:34:29 +0200

    Add device alias to the db

--- db_helper.py
+++ db_helper.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 class DBHelper:
-    def __init__(self, dbname="todo.sqlite",):
+    def __init__(self, dbname="todo.sqlite", ):
         self.dbname = dbname
         self.conn = sqlite3.connect(dbname)
 
@@ -15,7 +15,7 @@ class DBHelper:
         self.conn.close()
 
     def _setup_rel(self):
-        stmt = "CREATE TABLE IF NOT EXISTS userRel (device text, id text)"
+        stmt = "CREATE TABLE IF NOT EXISTS userRel (device text, device_name text, id text)"
         self.conn.execute(stmt)
         self.conn.commit()
 
@@ -24,16 +24,22 @@ class DBHelper:
         self.conn.execute(stmt)
         self.conn.commit()
 
-    def add_user(self, device, id):
-        self.delete_user(device, id)
-        stmt = "INSERT INTO userRel (device, id) VALUES (?,?)"
+    def add_user(self, device, device_name, id):
+        self.delete_user_by_id(device, id)
+        stmt = "INSERT INTO userRel (device,device_name, id) VALUES (?,?,?)"
+        args = (device, device_name, id)
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def delete_user_by_id(self, device, id):
+        stmt = "DELETE FROM userRel WHERE device = (?) AND id = (?)"
         args = (device, id)
         self.conn.execute(stmt, args)
         self.conn.commit()
 
-    def delete_user(self, device, id):
-        stmt = "DELETE FROM userRel WHERE device = (?) AND id = (?)"
-        args = (device, id)
+    def delete_user_by_id_and_device_name(self, device_name, id):
+        stmt = "DELETE FROM userRel WHERE id = (?) AND device_name=(?)"
+        args = (id, device_name)
         self.conn.execute(stmt, args)
         self.conn.commit()
 
@@ -59,6 +65,19 @@ class DBHelper:
         args = (device,)
         return [x[0] for x in self.conn.execute(stmt, args)]
 
+    def get_device_names_by_chatID(self, id):
+        stmt = "SELECT DISTINCT device_name FROM userRel where id = (?)"
+        args = (id,)
+        return [x[0] for x in self.conn.execute(stmt, args)]
+
+    def get_device_name_by_chatID_and_device(self, id, device):
+        stmt = "SELECT DISTINCT device_name FROM userRel where id = (?) AND device= (?)"
+        args = (id, device)
+        cursor = self.conn.execute(stmt, args).fetchone()
+        if cursor is None:
+            return None
+        return cursor[0]
+
     def get_feedback_by_target(self, target):
         stmt1 = "SELECT count(*) FROM userFeedback where target = (?) AND unwanted == 1 group by target"
         stmt2 = "SELECT count(*) FROM userFeedback where target = (?) AND unwanted == 0 group by target"
@@ -74,4 +93,4 @@ class DBHelper:
         elif ret2 is None:
             return ret1[0], 0
         else:
-            return ret1[0],ret2[0]
+            return ret1[0], ret2[0]
