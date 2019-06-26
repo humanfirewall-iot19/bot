069e618b92600e73651d49f881f6c645f9ecc847
Leonardo Sarra <leonardosarra@outlook.com>
2019-06-26 22:17:14 +0200

    Fix stop and error printing

--- queue_publisher.py
+++ queue_publisher.py
@@ -4,24 +4,26 @@ import paho.mqtt.client as mqtt
 import json
 import time
 import configparser
+
+
 class QueuePublisher:
 
     def __init__(self, ip):
-        self.client = mqtt.Client() 
-        print("connecting to broker ",ip)
-        self.client.connect(ip,1883)
+        self.client = mqtt.Client()
+        print("connecting to broker ", ip)
+        self.client.connect(ip, 1883)
         self.client.loop_start()
 
-    def publishResults(self,encoding,isUnwanted,chat_id,time):
+    def publishResults(self, encoding, isUnwanted, chat_id, time):
         data = {}
         data["encoding"] = encoding
         data["isUnwanted"] = isUnwanted
         data["chat_id"] = chat_id
         data["time"] = time
-        data_out=json.dumps(data) # encode object to JSON
-        ret=self.client.publish("masterResults",data_out)
-        print("sent ",data_out,ret)
+        data_out = json.dumps(data)  # encode object to JSON
+        ret = self.client.publish("masterResults", data_out)
+        print("sent ", data_out, "error code", ret[0])
 
     def stop(self):
-        client.disconnect() #disconnect
-        client.loop_stop() #stop loop
+        self.client.disconnect()  # disconnect
+        self.client.loop_stop()  # stop loop
